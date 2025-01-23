6f13c9355d41639410a6b10130a08884347bc9d7
Михаил Капелько <kornerr@gmail.com>
2025-01-23 22:45:11 +0300

    d

--- src/fs_jvm.kt
+++ src/fs_jvm.kt
@@ -10,6 +10,26 @@ package org.opengamestudio
 import java.io.*
 
 /**
+ * List files of the provided directory
+ *
+ * JVM implementation
+ */
+fun fsListFiles(path: String): Array<FSFile> {
+    var items = arrayOf<FSFile>()
+    val list = File(path).listFiles()
+    for (file in list) {
+        val item =
+            FSFile(
+                file.isDirectory(),
+                file.isFile(),
+                file.getName(),
+            )
+        items += item
+    }
+    return items
+}
+
+/**
  * Read text file
  *
  * JVM implementation
