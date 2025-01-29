a91490af4c4a85f7a86ff16ef384b0f94a9d25ac
Михаил Капелько <kornerr@gmail.com>
2025-01-29 22:17:33 +0300

    d

--- src/constants.kt
+++ src/constants.kt
@@ -10,6 +10,7 @@ package org.opengamestudio
 const val ARGUMENT_CFG = "--cfg"
 const val ARGUMENT_DBG = "--dbg"
 const val CFG_FILE = "pskov.cfg"
+const val DBG_LEN = 50
 const val FS_DELIMITER = "/"
 const val PAGE_DATE = "Date:"
 const val PAGE_SLUG = "Slug:"
